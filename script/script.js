d47d15e34ef612ca49a1a9a6f72a7983d1ca6cb1
dharmy05 <dharmy.dev05@gmail.com>
2025-04-15 19:24:49 +0100

    adding a label for type of transaction in html file with basic styling in css

--- script/script.js
+++ script/script.js
@@ -6,7 +6,8 @@ const transactionRecord = document.querySelector(".transaction_record");
 const submitBtn = document.querySelector(".submit-btn");
 const ulListItem = document.querySelector(".transactonList");
 const transactionStatus = document.querySelector("#status");
-const form = document.querySelector("form")
+const form = document.querySelector("#form")
+console.log(form)
 
 const transaction = [
   {
@@ -31,46 +32,61 @@ const transaction = [
     type: "expense",
   },
 ];
+
+// Formats
+// Currency format
 const currencyFormatter = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
   signDisplay: "always",
 });
 
+// Date format
+const dateFormat = new Intl.DateTimeFormat('en-GB',{
+  year: "numeric",
+  month: "2-digit",
+  day:"2-digit"
+})
+
 const renderList = () => {
   if (transaction.length === 0) {
     transactionStatus.textContent = "No transactions";
     return;
   }
-  //  ulListItem.innerHTML =`<li>item</li>`
+ 
 
   transaction.forEach(({ id, name, amount, date, type }) => {
     listItem.innerHTML += `
     <div class="name">
-    <h2>${name}</h2>
-    <p>${new date(date).toLocaleDateString()}</p>
+    <h2 class="name-h2">${name}</h2>
+    <p class='date'>${dateFormat.format(date)}</p>
     </div>
       <div class="amount">
      <h2>${currencyFormatter.format(amount)}</h2>
+    //  <span class></span>
+    //  <span></span>
     </div>
 `;
   });
 };
 
-const addFormData = (e) => {
-  e.preventDefault()
+const addTransactions = () => {
  const formData = new FormData(this)
 
   transaction.push[{
     id: transaction.length + 1,
     name: formData.get('name'),
     amount: parseFloat(formData.get('amount')),
-    date: new date(formData.get('date')) 
+    date: new date(formData.get('date')),
+    type:"on"=== formData.get('type') ? "income":"expense"
  }]
   this.reset()
   renderList()
 }
 
-form.addEventListener("submit",addFormData())
-submitBtn.addEventListener("click", renderList);
+form.addEventListener("submit", function (e) {
+  e.preventDefault()
+  addTransactions()
+})
+submitBtn.addEventListener("click", renderList());
 
